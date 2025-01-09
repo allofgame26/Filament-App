968f4396bb36cb941b0edb88245bbd440f3aa8db
allofgame26 <rizkiarya79@yahoo.com>
2025-01-09 23:04:29 +0700

    membuat Notifikasi Sederhana seperti setelah membuat data baru

--- app/Filament/Resources/BarangResource/Pages/CreateBarang.php
+++ app/Filament/Resources/BarangResource/Pages/CreateBarang.php
@@ -5,8 +5,25 @@ namespace App\Filament\Resources\BarangResource\Pages;
 use App\Filament\Resources\BarangResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
 
 class CreateBarang extends CreateRecord
 {
     protected static string $resource = BarangResource::class;
+
+    // protected function getCreatedNotificationTitle(): ?string
+    // {
+    //     return 'Data Barang Sudah Tersimpan';
+    // }
+
+    protected function getCreatedNotification(): ?Notification
+{
+    return Notification::make()
+        ->success()
+        ->title('Data Sudah Tersimpan')
+        ->icon('heroicon-o-archive-box-arrow-down')
+        ->iconColor('success')
+        ->duration(2000) // mengatur durasi notifikasi, dalam milisecond. 2000 -> 2 detik
+        ->body('Data Barang sudah tersimpan.');
+}
 }
